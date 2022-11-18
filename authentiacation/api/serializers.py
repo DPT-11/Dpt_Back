ff2145f652e35d24871b29733c1ede24b3569619
seohyun <seohyun@naver.com>
2022-11-18 19:43:50 +0900

    USERLAST

--- authentiacation/api/serializers.py
+++ authentiacation/api/serializers.py
@@ -22,7 +22,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
         return User.objects.create_user(**validated_data)
 
 class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=10,read_only = True)
+    username = serializers.CharField(max_length=10)
     password = serializers.CharField(max_length=4,write_only = True)
     last_login = serializers.CharField(max_length =255, read_only=True)
     token = serializers.CharField(max_length=255, read_only=True)
